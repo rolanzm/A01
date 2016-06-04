deaa90c255d29ef7943f31466dbf9efa97b939f1
rolan <rolanzm@hotmail.com>
2016-06-04 15:51:40 -0500

    Cambio luego de agregar la librería

---
+++ ClassLibrary1/clsCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class clsCliente
+    {
+        private string _Nombre;
+
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+
+        
+    }
+}
